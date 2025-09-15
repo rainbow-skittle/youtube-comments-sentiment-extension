0625e76921d84215ba9798ef02b6519b88eee87b
rainbow-skittle <127092520+NDPriyanka@users.noreply.github.com>
2025-09-15 14:09:16 +0530

    updated

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -6,7 +6,7 @@ import logging
 import os
 
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri("http://ec2-52-91-160-21.compute-1.amazonaws.com:5000/")
+mlflow.set_tracking_uri("http://ec2-34-227-7-97.compute-1.amazonaws.com:5000/")
 
 
 # logging configuration
